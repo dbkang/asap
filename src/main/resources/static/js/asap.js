89eed479bf5c5784544b2ca5a6fce025eeabd8eb
Dan Kang <dartdanfm@gmail.com>
2014-01-19 21:52:28 -0500

    Basic routes and templte stubs for blogging module

--- src/main/resources/static/js/asap.js
+++ src/main/resources/static/js/asap.js
@@ -6,9 +6,30 @@
   function routeConfig($stateProvider, $urlRouterProvider, $locationProvider) {
     $locationProvider.html5Mode(true).hashPrefix('!');
     $urlRouterProvider.otherwise('/home');
+
     $stateProvider.state('home', {
       url: '/home',
       template: '<h1>HOME!</h1>'
+    });
+
+    $stateProvider.state('blog', {
+      url: '/blog',
+      templateUrl: '/static/partials/blog.html'
+    });
+
+    $stateProvider.state('blog.post', {
+      url: '/post',
+      templateUrl: '/static/partials/blog.post.html'
+    });
+
+    $stateProvider.state('blog.view', {
+      url: '/view/:id',
+      templateUrl: '/static/partials/blog.view.html'
+    });
+
+    $stateProvider.state('blog.edit', {
+      url: '/edit/:id',
+      templateUrl: '/static/partials/blog.edit.html'
     });
   }
 
