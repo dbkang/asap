a19ef905877858be7e3a1ec956e25fb116f16441
Dan Kang <dartdanfm@gmail.com>
2014-01-28 00:07:40 -0500

    Fixed bugs in blog.edit

--- src/main/resources/static/js/controllers/blog.edit.js
+++ src/main/resources/static/js/controllers/blog.edit.js
@@ -1,13 +1,13 @@
 'use strict';
 
 (function () {
-  angular.module('asap').controller('BlogEdit', ['$scope', '$state', '$stateParams', BlogEdit]);
-  function BlogEdit($scope, $state, $stateParams) {
+  angular.module('asap').controller('BlogEdit', ['$scope', '$state', '$stateParams', '$http', BlogEdit]);
+  function BlogEdit($scope, $state, $stateParams, $http) {
     $scope.post = {};
     $scope.post.title = "New Title";
     $scope.post.body = "Type body here";
 
-    $http.get('/api/autostore/default/blog/' + $stateParms.id).then(function(response) {
+    $http.get('/api/autostore/default/blog/' + $stateParams.id).then(function(response) {
       $scope.post = response.data;
     });
 
